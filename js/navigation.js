97a4d7f2946d2d20ffee54be0aa8974dda11c02c
Kevin Attfield <k.attfield@gmail.com>
2013-09-24 15:54:25 -0700

    added grunt tasks

--- js/navigation.js
+++ js/navigation.js
@@ -1,44 +1,47 @@
-(function($){
+/*global jQuery*/
+(function($) {
 	$(document).ready(function() {
-		console.log($(window).width())
-		if($(window).width() >= 768) {
+		if ($(window).width() >= 768) {
 			$('#site-navigation').affix({
-				offset: { top: $('#site-navigation').offset().top }
+				offset: {
+					top: $('#site-navigation').offset().top
+				}
 			});
 		}
 	});
 }(jQuery));
+
 /**
  * navigation.js
  *
  * Handles toggling the navigation menu for small screens.
  */
-( function() {
+(function() {
 	var container, button, menu;
 
-	container = document.getElementById( 'site-navigation' );
-	if ( ! container )
+	container = document.getElementById('site-navigation');
+	if (!container)
 		return;
 
-	button = container.getElementsByTagName( 'h1' )[0];
-	if ( 'undefined' === typeof button )
+	button = container.getElementsByTagName('h1')[0];
+	if ('undefined' === typeof button)
 		return;
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+	menu = container.getElementsByTagName('ul')[0];
 
 	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
+	if ('undefined' === typeof menu) {
 		button.style.display = 'none';
 		return;
 	}
 
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+	if (-1 === menu.className.indexOf('nav-menu'))
 		menu.className += ' nav-menu';
 
 	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) )
-			container.className = container.className.replace( ' toggled', '' );
+		if (-1 !== container.className.indexOf('toggled'))
+			container.className = container.className.replace(' toggled', '');
 		else
 			container.className += ' toggled';
 	};
-} )();
+})();
